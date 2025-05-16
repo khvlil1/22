19b028bd37f089d9fa8d10939c7864ad1a605abe
khvlil1 <20231700333@cis.asu.edu.eg>
2025-05-16 03:09:39 +0300

    Update Administrator.h

--- Administrator.h
+++ Administrator.h
@@ -1,1 +1,31 @@
+#pragma once
+#include <unordered_map>
+#include "User.h"
+#include "Course.h"
+#include "Student.h"
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+struct Instructor {
+    string firstName;
+    string lastName;
+};
+
+struct Semester {
+    string studentID;
+    string courseID;
+    float grade;
+};
+class Administrator : public User
+{
+  public:
+      Administrator(); /////////////////////////////////////////////////////////////////////////////////
+
+    Administrator(string u, string p);
+    void uploadCourse();
+    void setPrerequisites();
+    void manageStudentGrades(unordered_map<string, Student>& students, const unordered_map<string, Course>& availableCourses);
+};
 
