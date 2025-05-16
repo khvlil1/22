a6bf3b5c390b0ce737a6b1447aabbb8a67e35a99
khvlil1 <20231700333@cis.asu.edu.eg>
2025-05-16 19:39:29 +0300

    Update User.h

--- User.h
+++ User.h
@@ -4,6 +4,12 @@
 #include <iostream>
 #include "Course.h"
 using namespace std;
+struct SignupInfo {
+    std::string username;
+    std::string password;
+    std::string role;
+};
+
 class User
 {
 protected:
@@ -14,14 +20,16 @@ private:
     unordered_map<string, Course> courses;
 public:
     User();
-    User(string u, string p, string r = "");
+    User(const string& username, const string& password, const string& role);
     unordered_map<string, Course> getCourses() const;
     void setCourses(const unordered_map<string, Course>& newCourses);
+    void setUsername(const string& username);
+    void setPassword(const string& password);
     string getUsername() const;
     string getPassword() const;
     string getRole() const;//////////////////////////////////////////////////////
     void setRole(string r);///////////////////////////
     static void signUp(unordered_map<string, User>& users);
     static string signIn(const unordered_map<string, User>& users);
+    static SignupInfo signUp(const unordered_map<string, User>& users);
 };
-
