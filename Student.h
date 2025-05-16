0efe8b90104e46b08e9f95dcc392cb735d2f38be
khvlil1 <20231700333@cis.asu.edu.eg>
2025-05-16 03:15:24 +0300

    Update Student.h

--- Student.h
+++ Student.h
@@ -1,1 +1,52 @@
+#pragma once
+#include <iostream>
+#include<stack>
+#include <unordered_map>
+#include <vector>
+#include "User.h"
+#include "Course.h"
+using namespace std;
 
+class Student :public User
+{
+private:
+    string StudentId;
+    string firstName;
+    string lastName;
+    string email;
+    string password;
+    int academicYear;
+    string major;
+    unordered_map<string, Course> registeredCourses;
+    unordered_map<string, float> grades;
+    stack<string> CurrentRegistration;
+
+public:
+    Student();
+    Student(string username, string password,string role);
+    void setStudentID(const string& id);
+    void setStudentName(const string& fname, const string& lname);
+    void setEmail(const string& email);
+    void setPassword(const string& pwd);
+    void setAcademicYear(int year);
+    void setMajor(const string& major);
+
+    string getStudentID() const;
+    string getStudentfName() const;
+    string getStudentlName() const;
+    string getEmail() const;
+    string getPassword() const;
+    int getAcademicYear() const;
+    string getMajor() const;
+    unordered_map<std::string, float> getGrades() const;
+
+
+    void setGrade(const string& courseID, float grade);
+    void ViewStudentGrades();
+
+
+    void setRegisteredCourses(const unordered_map<string, Course>& courses);
+    unordered_map<string, Course> getRegisteredCourses() const;
+    void Search(const string& courseID, const unordered_map<string, Course>& availableCourses);
+    bool RemoveRegistration();
+};
