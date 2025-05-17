61656057bfae5a25fec75bb831d639468090f074
khvlil1 <20231700333@cis.asu.edu.eg>
2025-05-17 04:54:29 +0300

    Update User.h

--- User.h
+++ User.h
@@ -15,20 +15,22 @@ class User
 protected:
     string username;
     string password;
-    string role;  // "A" or "S"///////////////////////////////////////////////////////////////////////
+    string role;  // "A" or "S"
 private:
     unordered_map<string, Course> courses;
 public:
     User();
     User(const string& username, const string& password, const string& role);
-    unordered_map<string, Course> getCourses() const;
-    void setCourses(const unordered_map<string, Course>& newCourses);
+    unordered_map<string, Course>& getCourses();
+    
+
+    void setCourses(unordered_map<string, Course>& newCourses);
     void setUsername(const string& username);
     void setPassword(const string& password);
     string getUsername() const;
     string getPassword() const;
-    string getRole() const;//////////////////////////////////////////////////////
-    void setRole(string r);///////////////////////////
+    string getRole() const;
+    void setRole(string r);
     static void signUp(unordered_map<string, User>& users);
     static string signIn(const unordered_map<string, User>& users);
     static SignupInfo signUp(const unordered_map<string, User>& users);
